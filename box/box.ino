531a555c9a7c25fdbff2bdd68dc9bed6ec39f895
ryplo <lo.rachel8@gmail.com>
2018-03-09 15:42:53 +0800

    rotates once when ground and sensor triggered

--- box/box.ino
+++ box/box.ino
@@ -1,12 +1,23 @@
 #include <CapacitiveSensor.h>
+#include <AccelStepper.h>
 
-CapacitiveSensor cap = CapacitiveSensor(6,7);
-//pin 7 is receiving pin
-unsigned long csSum;
+#define FULLSTEP 4 //half step
+// Motor pins
+#define motor1  8 // IN1 on the driver
+#define motor2  9 // IN2 on the driver
+#define motor3  10 // IN3 on the driver
+#define motor4  11 // IN4 on the driver
+
+AccelStepper stepper1(FULLSTEP, motor1, motor3, motor2, motor4);
+
+CapacitiveSensor cap = CapacitiveSensor(6,7); //pin 7 is receiving pin
 
 void setup() {
   Serial.begin(9600);
   pinMode(6, OUTPUT);
+  stepper1.setMaxSpeed(1000.0);
+  stepper1.setAcceleration(100.0);
+  stepper1.setSpeed(300);
 }
 
 void loop() {
@@ -15,5 +26,9 @@ void loop() {
   delay(10);
   if (val >= 4000) {
     Serial.println("Triggered");
+    long curr = stepper1.currentPosition();
+    long goal = curr + 4096/2;
+    stepper1.runToNewPosition(goal);
+    Serial.println("one rotation");
   }
 }
